5088f4560c311853888bbce0d55a58a158eeb1c1
andreisc <andrei.castro1991@gmail.com>
2023-08-26 09:41:36 -0300

    First commit

---
+++ AppTestNUnit/Conta.cs
@@ -0,0 +1,30 @@
+﻿namespace AppTestNUnit
+{
+    public class Conta
+    {
+        #region Atribuitos
+        private string cpf;
+        private decimal saldo;
+        #endregion Atribuitos
+
+        public Conta(string _cpf, decimal _valor)
+        {
+            cpf = _cpf;
+            saldo = _valor;
+        }
+
+        public decimal Saldo()
+        {
+            return saldo;
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if(saldo < valor)
+                return false;
+
+            saldo -= valor;
+            return true;
+        }
+    }
+}
---
+++ AppTestNUnit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppTestNUnit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
